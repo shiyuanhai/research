ffcc9587f2f5a25065691770435bd69235ee689b
Yuanhai <shiyuanhai@gmail.com>
2016-10-18 23:53:04 -0400

    [webpack]getting started
    http://webpack.github.io/docs/usage.html

---
+++ webpack/getting_started/webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: './bin',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({//535 -> 174kb
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ]
+};
